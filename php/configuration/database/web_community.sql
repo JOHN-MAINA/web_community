bdbf13f9d6179d536bdb48fd1e30da11d74b4633
John Maina <jqhnmaina@outlook.com>
2018-02-16 15:23:07 +0300

    Added web_community slq file

---
+++ php/configuration/database/web_community.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 16, 2018 at 10:22 AM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `web_community`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(20) NOT NULL,
+  `sname` varchar(20) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `fname`, `sname`, `email`) VALUES
+(1, 'user1Fname', 'user1Sname', 'user1@email.com'),
+(2, 'user2Fname', 'user2Sname', 'user2@email.com'),
+(3, 'user3Fname', 'user3Sname', 'user3@email.com'),
+(4, 'user4Fname', 'user4Sname', 'user4@email.com'),
+(5, 'user5Fname', 'user5Sname', 'user5@email.com');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- php/configuration/env.php
+++ php/configuration/env.php
@@ -2,6 +2,6 @@
     define('USER_NAME',  'root');
     define('DB_NAME', 'web_community');
     define('HOST', 'localhost');
-    define('PASS', 'gemini');
+    define('PASS', 'yourpassword');
 
  ?>
